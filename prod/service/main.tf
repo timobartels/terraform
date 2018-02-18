aa9bf1ba18ccfc7bf331d4c46d7299cf3c164f97
Timo Bartels <timo@bartels-online.net>
2018-02-18 14:03:31 +0100

    introducing modules

--- prod/service/main.tf
+++ prod/service/main.tf
@@ -2,7 +2,7 @@ terraform {
     required_version = ">= 0.10, < 0.12"
     backend "s3" {
         region   = "us-east-1"
-        key      = "sample/stage/service/terraform.tfstate"
+        key      = "sample/prod/service/terraform.tfstate"
         bucket   = "tbartels-terraform-state"
     }
 }
@@ -14,9 +14,9 @@ provider "aws" {
 module "webserver" {
    source = "../../modules/services/webserver"
 
-   cluster_name = "test-module-cluster"
+   cluster_name = "prod-module-cluster"
    ec2_instance_count = 1
-   ec2_instance_name = "dev-instance"
+   ec2_instance_name = "prod-instance"
    ec2_instance_type = "t2.micro"
    server_port = "${var.server_port}"
 }
