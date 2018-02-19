4d49a888591096645c9826268bf820366c90fdd8
timobartels <timobartels@gmx.de>
2018-02-19 17:17:12 +0100

    Terraform modules (#1)
    * introducing modules
    
    * removing plan file
    
    * refactored to use modules from separate repo

--- prod/service/main.tf
+++ prod/service/main.tf
@@ -2,7 +2,7 @@ terraform {
     required_version = ">= 0.10, < 0.12"
     backend "s3" {
         region   = "us-east-1"
-        key      = "sample/stage/service/terraform.tfstate"
+        key      = "sample/prod/service/terraform.tfstate"
         bucket   = "tbartels-terraform-state"
     }
 }
@@ -13,10 +13,10 @@ provider "aws" {
 
 module "webserver" {
    source = "git::git@github.com:timobartels/terraform-modules.git//services/webserver"
-	
-   cluster_name = "test-module-cluster"
+
+   cluster_name = "prod-module-cluster"
    ec2_instance_count = 1
-   ec2_instance_name = "dev-instance"
+   ec2_instance_name = "prod-instance"
    ec2_instance_type = "t2.micro"
    server_port = "${var.server_port}"
 }
